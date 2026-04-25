9bb8d1c74b8077dcf9ce33f7f5c50944a9bf2816
Lucas Mendes <lucas.silva0020015704@edu.pe.senac.br>
2026-04-25 18:28:47 -0300

    Update service-worker.js

--- frontend/service-worker.js
+++ frontend/service-worker.js
@@ -1,22 +1,25 @@
-const CACHE_NAME = 'livros-cache-v1';
-const APP_FILES = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE_NAME = "livros-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
